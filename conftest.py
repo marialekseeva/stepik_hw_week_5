cb6563f49737b16c5ec79fd64c5cbdcd888734c8
marialekseeva <catastroficmaker@gmail.com>
2020-03-21 04:25:09 +0500

    Added test for registration user

--- conftest.py
+++ conftest.py
@@ -1,1 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose lang")
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    user_language = request.config.getoption("language")
+    print("\nstart browser chrome for test...")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser...")
+    browser.quit()
--- test_make_report.py
+++ test_make_report.py
@@ -1,1 +1,23 @@
+import random
 
+
+def test_registration(browser):
+    # Проверка первичной регистрации посетителя страницы, наличия фразы "Спасибо за регистрацию!" на конечной
+    # странице регистрации
+        random_email = "ghfjj@j%s.ru" % random.randint(1, 10000)
+    # self.random_email = "ghfjj@j123.ru"
+        #self.password = "Alekseevaa
+        link = "http://selenium1py.pythonanywhere.com/ru/"
+        browser.get(link)
+        registration = browser.find_element_by_id("login_link")
+        registration.click()
+        input1 = browser.find_element_by_name("registration-email")
+        input1.send_keys(random_email)
+        input2 = browser.find_element_by_name("registration-password1")
+        input2.send_keys("48g3ui48g3ui48g3ui")
+        input3 = browser.find_element_by_name("registration-password2")
+        input3.send_keys("48g3ui48g3ui48g3ui")
+        button = browser.find_element_by_name("registration_submit")
+        button.click()
+        assert browser.find_element_by_class_name("alertinner").is_displayed(), \
+    'Регистрация прошла неуспешно!'
